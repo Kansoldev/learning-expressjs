09b28b7cf532c0be416cdfa53a19f5cbdc0cb7c3
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-21 16:01:48 +0100

    docs: creating routes in Express
    Routes are created in express by specifying the type of request as a method e.g get(), post(), patch() etc. Each request type takes in 2 parameters, the url of the request, and a function that takes in 2 parameters of it's own which are request and response, prefixed as `req, res`. This request and response parameters give a ton of information I can use to get data about the request been made, and the kind of response to send to the client.
    
    Another nice thing about using this request type methods is that I don't have to specify the content type of the response. Depending on how my response is structured, maybe I insert HTML code into the response for example, Express will automatically figure out the right content type and insert it so the content displays the way it should.

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const express = require("express");
 // Initialize express
 const app = express();
 
+// Creating routes
+app.get("/", (req, res) => {
+  //   res.send("Hello World");
+  //   res.send({
+  //     id: 1,
+  //     name: "Oyinkansola",
+  //   });
+  res.send("<h1>Hello World</h1>");
+});
+
 app.listen(5000, () => console.log("Server running"));
