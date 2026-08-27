5b2d552454dfc71f4b4913bb80fcc525b4cd6524
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-27 14:16:28 +0100

    docs: The better way for loading static files, especially when they are plenty
    Declaring a folder as static tells Express that anything placed inside that folder is not dynamic in anyway, so it doesn't need to do any processing, just send the file. In this case, "public" is used as the static folder. A folder is declared as static using the `static()` method in Express, this method also takes in the absolute path to the folder.
    
    The one thing to note though is that when accessing static files this way like html files, I cannot omit the extension, else, the browser won't read the file when I go to the route for that page e.g /about will fail, I must do /about.html. The reason why this doesn't work really is because I never explicitly specified `/about` as a route, so Express won't know what to load when that route is hit, hence why I have to specify /about.html

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const path = require("path");
 // Initialize express
 const app = express();
 
-// Creating routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+// Setup static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(5000, () => console.log("Server running"));
