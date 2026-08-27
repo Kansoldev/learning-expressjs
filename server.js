e6be5b74ba0308facd9042996ffafad0fb404cd6
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-27 15:00:14 +0100

    docs: how to work with JSON in Express
    When sending JSON data in Express, it has a specific method called `json()`. There's no need to use res.send() when working with JSON data.

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Setting up a static server in Express for loading static files
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// Using static posts data
+let posts = [
+  {
+    id: 1,
+    title: "Post one",
+  },
+  {
+    id: 2,
+    title: "Post two",
+  },
+  {
+    id: 3,
+    title: "Post three",
+  },
+];
+
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  // Specific json method for sending data as json
+  res.json(posts);
+});
 
 app.listen(port, () => console.log("Server running"));
