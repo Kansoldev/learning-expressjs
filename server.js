65eaff853fe227f8bc5ecbff370ee2fd755e347e
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-27 14:05:20 +0100

    docs: one way to handle loading files with Express
    This is not very efficient, especially scalability wise because creating a route to load several files does not really make sense, it will only lead to unnecessary repeated code, there's a much better way to handle that sort of situation. This is best if the files I want to load aren't much.
    
    The `sendFile()` does exactly has the name implies, it sends a file to the client. It takes in the absolute path to the file as a parameter. I can create this absolute path using the `path` module in Node, and using the `join()` method to join the paths that describe the location of the file. `__dirname` returns the current location of the file where __dirname is being used.

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 // Initialize express
 const app = express();
 
 // Creating routes
 app.get("/", (req, res) => {
-  //   res.send("Hello World");
-  //   res.send({
-  //     id: 1,
-  //     name: "Oyinkansola",
-  //   });
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 app.listen(5000, () => console.log("Server running"));
