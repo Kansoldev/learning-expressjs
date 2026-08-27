e64d2362bd0ff851a4828e346c42bdbfd71ba66a
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-27 16:47:54 +0100

    refactor: moving routes to a routes folder
    In a real world application, I won't have all my routes in one place, it will be too messy, so the convention is to have those routes in a folder, and depending on the data being fetched, the routes will have their own files.
    
    There is a `Router()` method that comes with Express used for routing that is mostly used for more flexible routing, instead of just calling get(), post() etc. Another thing to note is anytime I am using the `use()` method in Express, it indicates I am using a middleware, and the `router()` method in Express is one middleware.
    
    The most important benefit of using the `Router()` method is that when I use it in my main express app, specifying the main route path, I don't have to redefine that route again where it's been used, I only have to specify the part of the url that is attached to the route.

---
+++ routes/posts.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+// Using static posts data
+let posts = [
+  {
+    id: 1,
+    title: "Post one",
+  },
+  {
+    id: 2,
+    title: "Post two",
+  },
+  {
+    id: 3,
+    title: "Post three",
+  },
+];
+
+// Get all posts
+router.get("/", (req, res) => {
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+
+  res.status(200).json(posts);
+});
+
+// Get a single post
+router.get("/:id", (req, res) => {
+  /*
+    Get the id passed from the url. req.params returns a string,
+    so using parseInt() converts it into a number
+  */
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({
+      msg: "Post not found",
+    });
+  }
+
+  res.status(200).json(post);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const posts = require("./routes/posts");
 const port = process.env.PORT || 8080;
 
 // Initialize express
@@ -8,49 +9,7 @@ const app = express();
 // Setting up a static server in Express for loading static files
 // app.use(express.static(path.join(__dirname, "public")));
 
-// Using static posts data
-let posts = [
-  {
-    id: 1,
-    title: "Post one",
-  },
-  {
-    id: 2,
-    title: "Post two",
-  },
-  {
-    id: 3,
-    title: "Post three",
-  },
-];
-
-// Get all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-
-  res.status(200).json(posts);
-});
-
-// Get a single post
-app.get("/api/posts/:id", (req, res) => {
-  /*
-    Get the id passed from the url. req.params returns a string,
-    so using parseInt() converts it into a number
-  */
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-
-  if (!post) {
-    return res.status(404).json({
-      msg: "Post not found",
-    });
-  }
-
-  res.status(200).json(post);
-});
+// Using the posts route
+app.use("/api/posts", posts);
 
 app.listen(port, () => console.log("Server running"));
