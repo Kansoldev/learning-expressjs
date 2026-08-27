6c1e96d26c11b7d772ed609d342496780029bd05
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-27 16:12:51 +0100

    docs: using status codes in Express
    `find()` ends the loop when it gets the first match that matches the condition, unlike `filter()` that must run through the whole array even through it might have gotten a match already. So `find()` is more efficient in cases like this where I just need a single piece of data

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.get("/api/posts", (req, res) => {
   const limit = parseInt(req.query.limit);
 
   if (!isNaN(limit) && limit > 0) {
-    res.json(posts.slice(0, limit));
-  } else {
-    res.json(posts);
+    return res.status(200).json(posts.slice(0, limit));
   }
+
+  res.status(200).json(posts);
 });
 
 // Get a single post
@@ -42,7 +42,15 @@ app.get("/api/posts/:id", (req, res) => {
     so using parseInt() converts it into a number
   */
   const id = parseInt(req.params.id);
-  res.json(posts.filter((post) => post.id === id));
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({
+      msg: "Post not found",
+    });
+  }
+
+  res.status(200).json(post);
 });
 
 app.listen(port, () => console.log("Server running"));
